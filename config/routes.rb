82174050c31ce32941f7aec310e83834ac86bc69
rolandm99 <manfulmweze99@gmail.com>
2022-03-29 13:36:37 +0100

    Setup project and add configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'splash/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'splash#index'
 end
