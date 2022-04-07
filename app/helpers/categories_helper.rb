137c6d3a3a89fa546d82adc476d47157ff729c96
rolandm99 <manfulmweze99@gmail.com>
2022-04-07 11:37:58 +0100

    Update models and helpers folder

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,6 @@
 module CategoriesHelper
+  def example_img(category)
+    img_example = '/assets/budgetbulga.png'
+    category.icon.present? ? category.icon : img_example
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  has_many :transactions
+  has_many :categories
 end
